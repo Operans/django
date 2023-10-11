db641928eefc3fed9d3a45998d82546991e03f62
Operans <chingiz.kartanbaev@gmail.com>
2023-10-11 20:38:31 +0600

    lab 2

--- book/models.py
+++ book/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+    GENRE = (
+        ('Жанр', 'Жанр'),
+        ('Боевик', 'Боевик'),
+        ('Романтика', 'Романтика'),
+    )
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='')
+    genre = models.CharField(max_length=100, choices= GENRE, default = GENRE[0],  null=True)
+    video = models.URLField(null=True)
     cost = models.IntegerField()
     created_date = models.DateTimeField(auto_created=True)
 
--- book/views.py
+++ book/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 # Create your views here.
 def book_view(request):
     book_value = models.Book.objects.all()
-    return render(request,"book.html",{"book_key":book_value})
+    return render(request,"book.html",{"book_key" : book_value})
+
+def book_detail_view(request, id):
+    book_id = get_object_or_404(models.Book, id = id)
+    return render(request, 'book_detail.html', {'book_key' : book_id})

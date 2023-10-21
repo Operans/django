abdf676cfb5196e5c5efac6f75a33a1929f5a7a3
Operans <chingiz.kartanbaev@gmail.com>
2023-10-21 10:06:43 +0600

    lab 3

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.book_view),
     path('book_detail/<int:id>/', views.book_detail_view),
+    path('book_list/', views.book_delete_view),
+    path('book_list/<int:id>/delete/', views.book_drop_view),
+    path('create_book/', views.createBookPostView),
+    path('add-cooment/', views.createBookReview),
 ]
 
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 # Create your views here.
 def book_view(request):
@@ -9,3 +10,36 @@ def book_view(request):
 def book_detail_view(request, id):
     book_id = get_object_or_404(models.Book, id = id)
     return render(request, 'book_detail.html', {'book_key' : book_id})
+
+def createBookPostView(request):
+    method = request.method
+    if method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Успешно добавлен')
+    else:
+        form = forms.BookForm()
+    return render(request, 'create_book.html', {'form': form})
+
+def book_delete_view(request):
+    lang_value = models.Book.objects.all()
+    return render(request, 'book_list.html', {'lang_key': lang_value})
+
+def book_drop_view(request, id):
+    lang_id = get_object_or_404(models.Book, id=id)
+    lang_id.delete()
+    return HttpResponse('Успешно удален')
+
+def createBookReview(requset):
+    method = requset.method
+    if method == 'POST':
+        form = forms.ReviewForm(requset.POST, requset.FILES)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponse('<h1>Success</h1>')
+    else:
+        form = forms.ReviewForm()
+
+    return render(requset, 'createReview.html', {'form' : form})

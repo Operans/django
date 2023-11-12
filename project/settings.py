bd0bad041dbb14b777c88600a96dbcdb79231a06
Operans <chingiz.kartanbaev@gmail.com>
2023-11-12 11:17:57 +0600

    register

--- project/settings.py
+++ project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'custom_users'
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('book.urls')),
     path('admin/', admin.site.urls),
+    path('', include('book.urls')),
+    path('', include('custom_users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
